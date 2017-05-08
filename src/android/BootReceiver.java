f6e1a5fac88549acba7a0f7b865a56f0ca11ea29
Rodolfo Nogueira <rodol@DESKTOP-HFCT547.(none)>
2017-05-07 21:35:48 -0300

    commit inicial

---
+++ src/android/BootReceiver.java
@@ -0,0 +1,17 @@
+package com.example.plugin;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import io.ionic.starter.MainActivity;
+
+
+public class BootReceiver extends BroadcastReceiver{
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent i = new Intent(context, MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+	}
+}
